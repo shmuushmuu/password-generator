3ba531f042c66aac539cad7bbad212fe73df8b74
Sam Craig <samfcraig@gmail.com>
2022-11-29 21:11:50 -0500

    fixed generate password() recall in end function

--- script.js
+++ script.js
@@ -3,27 +3,26 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword(){
   //YOUR CODE GOES HERE
-  var characters = [letters, upLetters, special, numbers];
+  var characters = letters + upLetters + specials + numbers;
   var letters = "abcdefghijklmnopqrstuvwxyz";
   var specials = "!#$%&()*+,-./:;<=>?@[\]^_`{|}~";
   var numbers = "1234567890";
   var upLetters = letters.toUpperCase;
-  var pwLengthRequest = prompt('This generator makes passwords between 8 and 128 characters. How many characters do you need?');
+  var pwLengthRequest = prompt("This generator makes passwords between 8 and 128 characters. How many characters do you need?");
+
+  pwLengthRequest();
 
   if (
-    passwordLength < 8 || passwordLength > 128
-    return pwLengthRequest;
+    passwordLength >= 8 || passwordLength <= 128
   )
-  } else {
-
-  }
+  {
 
   var isUpperCase = confirm("Would you like to include uppercase letters in your password?");
   var isLowerCase = confirm("Would you like to include lower case letters in your password?");
   var isSpecial = confirm("Would you like to include special characters in your password?");
   var isNumber = confirm("Would you like to include numbers in your password?");
-  var sum = 0;
-  for (var i = 1; i <= num, i++);
+  // var sum = 0;
+  // for (var i = 1; i <= num, i++);
   var random = Math.floor(Math.random() * characters.length);
 
 if (isUpperCase) {
@@ -51,11 +50,11 @@ if (isNumber) {
   console.log(password);
   return "your password here";
 }
+}
 
-//Write password to the #password inout
+//Write password to the #password input
 function writePassword() {
-
-  var password = generatePassword(letters, specials, numbers); {
+  var password = generatePassword(); {
 
   }
   var passwordText = document.querySelector("#password");
